88b1f12e8c622bcd7ab5ceece4c0f79a8c650de3
Grant Forsythe <grantwforsythe@gmail.com>
2024-02-23 11:47:34 -0500

    build: add environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  isProduction: false,
+  clientId: 'Ca1x3pokPncutx3Y3MjRJ0C1bWwvS7viGFzci3zB5pw',
+  redirectUri: 'http://localhost:4200/oauth',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  isProduction: true,
+  clientId: '76lE0uK-YxLemGq6eLnRrPYdAKYNbqyC_mFcInqi5d',
+  redirectUri: 'https://ynab-custom-reports.onrender.com/oauth',
+};
