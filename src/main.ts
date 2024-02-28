8c9d5fcdb8b105afe2fd5e79c3006b1866c2107b
Grant Forsythe <grantwforsythe@gmail.com>
2024-02-28 18:48:27 -0500

    chore: add eslint rules

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
