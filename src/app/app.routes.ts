19dadb782eca0c7698858443de9b55b6fef7892f
Grant Forsythe <grantwforsythe@gmail.com>
2024-02-28 21:24:32 -0500

    feat: add budgets component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { authGuard } from './services/auth/auth.guard';
+import { BudgetsComponent } from './budgets/budgets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'budgets', component: BudgetsComponent, canActivate: [authGuard] },
+];
---
+++ src/app/budgets/budgets.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budgets',
+  templateUrl: './budgets.component.html',
+})
+export class BudgetsComponent {}
