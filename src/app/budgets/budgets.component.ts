6c45a2dca3ba66b53186d1f24ece3a47af165dd7
Grant Forsythe <grantwforsythe@gmail.com>
2024-02-29 23:11:18 -0500

    feat: add cards for budgets

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { YnabService } from '../services/ynab/ynab.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-budgets',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatRippleModule, DatePipe],
   templateUrl: './budgets.component.html',
 })
-export class BudgetsComponent {}
+export class BudgetsComponent {
+  ynab = inject(YnabService);
+  budgets$ = this.ynab.getBudgets();
+
+  constructor() {}
+}
