b232aa3bc089ed3449ba63f4d3bd96075541ecb9
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-05 18:37:39 -0500

    fix: update client ID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   isProduction: true,
   auth: {
-    clientId: '76lE0uK-YxLemGq6eLnRrPYdAKYNbqyC_mFcInqi5d',
+    clientId: '76lE0uK-YxLemGq6eLnRrPYdAKYNbqyC_mFcInqi5dg',
   },
 };
