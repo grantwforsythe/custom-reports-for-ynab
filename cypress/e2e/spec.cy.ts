780939282247373674be890f325930850d2fac88
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-04 19:34:20 -0500

    test: add custom 'authenticate' cypress command
    Add custom command that sets an auth token in localstorage.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,17 @@
+// TODO: Write E2E tests
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+  beforeEach(() => {
+    cy.authenticate();
+  });
+
+  it('Should logout', () => {
     cy.visit('/');
+    cy.get('button').should('contain', 'Logout').click();
     cy.get('button').should('contain', 'Authenticate');
+  });
+
+  it('Visit budgets', () => {
+    cy.visit('/budgets');
+    cy.get('mat-card:nth-child(3)').should('contain', "Grant's Budget (CAD)");
   });
 });
