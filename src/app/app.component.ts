fd8c5da14526836233e73a729b97ed6eda32a805
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-06 23:08:53 -0500

    feat: add github icon
    Add GitHub icon and link to project.
    Fix the position of the footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, MatIconModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor() {}
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'github',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/images/github.svg'),
+    );
+  }
 }
