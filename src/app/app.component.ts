815a892093da072b8189fb7e18ad5075eef0e11a
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-08 15:47:55 -0500

    feat: replace header with toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, MatIconModule, MatDividerModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LoginComponent,
+    MatIconModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  auth = inject(AuthService);
+
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
       'github',
