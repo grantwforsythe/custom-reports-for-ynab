71635a1b1cad6a7bad04d1148c20d31ebbbcc5c4
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-08 15:50:37 -0500

    feat: add hover effect to budget cards

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -1,15 +1,16 @@
-import { Component, inject } from '@angular/core';
-import { YnabService } from '../services/ynab/ynab.service';
 import { CommonModule, DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { RouterModule } from '@angular/router';
+import { YnabService } from '../services/ynab/ynab.service';
 
 @Component({
   selector: 'app-budgets',
   standalone: true,
   imports: [CommonModule, MatCardModule, MatRippleModule, DatePipe, RouterModule],
   templateUrl: './budgets.component.html',
+  styleUrl: './budgets.component.scss',
 })
 export class BudgetsComponent {
   ynab = inject(YnabService);
