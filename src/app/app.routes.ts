a7eac6574b2a31d733ec5a2540771914e85a9486
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-11 21:42:28 -0400

    feat: add privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { authGuard } from './services/auth/auth.guard';
 import { BudgetsComponent } from './budgets/budgets.component';
 import { BudgetDetailsComponent } from './budget-details/budget-details.component';
 import { HomeComponent } from './home/home.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 // TODO: Privacy policy route
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'privacy', component: PrivacyComponent },
   // TODO: Refactor to use child routes
   { path: 'budgets', component: BudgetsComponent, canActivate: [authGuard] },
   { path: 'budgets/:id', component: BudgetDetailsComponent, canActivate: [authGuard] },
