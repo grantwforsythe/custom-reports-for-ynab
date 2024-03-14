2499b2af1b2ddc7dbe80806201b6f8e0b6a750f4
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-14 17:39:01 -0400

    feat: add FlagIconComponent

---
+++ src/app/reports/ui/flag/flag.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FlagIconComponent } from './flag.component';
+
+describe('FlagIconComponent', () => {
+  let component: FlagIconComponent;
+  let fixture: ComponentFixture<FlagIconComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FlagIconComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FlagIconComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should set flag color and label', () => {
+    component.flag = { color: 'orange', name: 'Impulse' };
+    fixture.detectChanges();
+
+    const path: HTMLElement = fixture.nativeElement.querySelector('#flag-pill-icon-fill > path');
+    const span: HTMLElement = fixture.nativeElement.querySelector('span.label');
+
+    expect(path.getAttribute('fill')).toBe(component.flag.color);
+    expect(span.textContent).toBe(component.flag.name);
+  });
+});
---
+++ src/app/reports/ui/flag/flag.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { FlagColor } from '../../../shared/services/ynab/interfaces/transactions/flagColor';
+
+@Component({
+  selector: 'app-charts-bar-vertical',
+  standalone: true,
+  templateUrl: './flag.component.html',
+})
+export class FlagIconComponent {
+  @Input({ required: true })
+  flag!: { color: FlagColor; name: string };
+
+  constructor() {}
+}
