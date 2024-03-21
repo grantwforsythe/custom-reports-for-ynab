d7d51a01aff5f70764d8c3ac11a5e523868c02ae
Grant Forsythe <grantwforsythe@gmail.com>
2024-03-20 21:06:18 -0400

    test: add spec for vertical bar chart component

---
+++ src/app/reports/feature/dashboard/charts/bar-vertical/bar-vertical.component.spec.ts
@@ -0,0 +1,81 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ChartsBarVerticalComponent } from './bar-vertical.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { CategoryGroup } from '../../../../../shared/services/ynab/interfaces/categories/categoryGroup';
+import { Account } from '../../../../../shared/services/ynab/interfaces/accounts/account';
+import { Transaction } from '../../../../../shared/services/ynab/interfaces/transactions/transaction';
+import { selectReportResults } from '../../../../data-access/report.selectors';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+describe('ChartsBarVerticalComponent', () => {
+  let component: ChartsBarVerticalComponent;
+  let fixture: ComponentFixture<ChartsBarVerticalComponent>;
+  let store: MockStore;
+
+  const initialState: {
+    categoryGroups: CategoryGroup[];
+    accounts: Account[];
+    transactions: Transaction[];
+  } = {
+    categoryGroups: [],
+    accounts: [],
+    transactions: [],
+  };
+
+  afterEach(() => {
+    store?.resetSelectors();
+  });
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        NgxChartsModule,
+        provideAnimationsAsync(),
+        provideMockStore({
+          initialState,
+          selectors: [
+            {
+              selector: selectReportResults,
+              value: [
+                {
+                  name: 'Groceries',
+                  value: 100,
+                },
+                {
+                  name: 'Pet Care',
+                  value: 25,
+                },
+                {
+                  name: 'Internet',
+                  value: 50,
+                },
+              ],
+            },
+          ],
+        }),
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChartsBarVerticalComponent);
+    component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should call populate chart on #ngOnInit()', () => {
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const legend: HTMLElement = fixture.nativeElement.querySelector('ul.legend-labels');
+    expect(legend.textContent).toContain('Groceries');
+    expect(legend.textContent).toContain('Pet Care');
+    expect(legend.textContent).toContain('Internet');
+  });
+});
