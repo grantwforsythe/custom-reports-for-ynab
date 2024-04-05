9b7a4f4e8c4c98018adb3aa493ee76a216799cfe
Grant Forsythe <grantwforsythe@gmail.com>
2024-04-04 21:47:14 -0400

    test: add e2e tests
    Add tests for the home, privacy, and budget pages.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,23 +1,73 @@
-describe('My First Test', () => {
+describe('Custom Reports for YNAB', () => {
   beforeEach(() => {
     cy.authenticate();
   });
 
-  it('Should logout', () => {
-    cy.visit('/');
-    cy.get('header button').should('contain', 'Logout').click();
-    cy.get('header button').should('contain', 'Authenticate');
+  afterEach(() => {
+    cy.logout();
   });
 
-  it('Should visit budgets', () => {
-    cy.visit('/budgets');
-    cy.get('main').contains("Grant's Budget (CAD)");
+  describe('Home', () => {
+    it('Should logout', () => {
+      cy.visit('/');
+      cy.get('header button').should('contain', 'Logout').click();
+      cy.get('header button').should('contain', 'Authenticate');
+    });
+
+    it('Should reroute to home page on 404', () => {
+      cy.visit('/bad-route');
+      cy.location().should((location) => expect(location.pathname).to.eq('/'));
+    });
   });
 
-  it('Should reroute to home page on 404', () => {
-    cy.visit('/bad-route');
-    cy.location().should((location) => {
-      expect(location.pathname).to.eq('/');
+  describe('Privacy', () => {
+    beforeEach(() => {
+      cy.log('Routing home');
+      cy.visit('/');
+    });
+
+    it('Should route to privacy page', () => {
+      cy.get('footer a').should('contain', 'Privacy Policy').click();
+      cy.location().should((location) => expect(location.pathname).to.eq('/privacy'));
+      cy.get('h1').should('contain', 'Privacy Policy');
+      cy.get('div.content a').should('have.attr', 'href', 'https://api.ynab.com/#terms');
+    });
+
+    it('Should access privacy page when not authenticated', () => {
+      cy.get('header button').should('contain', 'Logout').click();
+      cy.visit('/privacy');
+      cy.get('h1').should('contain', 'Privacy Policy');
+    });
+  });
+
+  describe('Budget', () => {
+    it('Should visit budgets', () => {
+      cy.visit('/');
+      cy.get('header a').click();
+      cy.location().should((location) => expect(location.pathname).to.eq('/budgets'));
+      cy.get('main').should('contain', "Grant's Budget (CAD)");
+    });
+
+    it('Should not be able to visit budgets when not authenticated', () => {
+      cy.visit('/');
+      cy.get('header button').should('contain', 'Logout').click();
+      cy.visit('/budgets');
+      cy.location().should((location) => expect(location.pathname).to.eq('/'));
+    });
+  });
+
+  describe('Dashboard', () => {
+    it('Should render vertical bar chart', () => {
+      cy.visit('/budgets');
+      cy.get('main').contains("Grant's Budget (CAD)").click();
+      // Check that the route ends with /dashboard
+      cy.location().should((location) => expect(location.pathname).to.match(/\/dashboard$/));
+      // cy.get('mat-select[formControlName=chartType]').click().get('mat-option').contains('Apple Inc.').click();
+      cy.get('form mat-select').first().contains('Vertical Bar Chart');
+      cy.get('ul.legend-labels > li').should((categories) => {
+        // TODO: Check that the number of categories in the legend is equal to the number of bars
+        expect(categories).to.have.length.greaterThan(0);
+      });
     });
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -5,10 +5,18 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     authenticate(): Chainable<any>;
+    logout(): Chainable<any>;
   }
 }
+
 Cypress.Commands.add('authenticate', () => {
   cy.log('Authenticating');
   window.localStorage.setItem('accessToken', Cypress.env('AUTH_TOKEN'));
   window.localStorage.setItem('expiresAt', (new Date().getTime() + 7200 * 1000).toString());
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.log('Logging out');
+  window.localStorage.removeItem('accessToken');
+  window.localStorage.removeItem('expiresAt');
 });
