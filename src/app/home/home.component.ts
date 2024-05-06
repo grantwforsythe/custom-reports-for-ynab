686b43fd72d636a2d0e94475274ce673c549742e
Grant Forsythe <grantwforsythe@gmail.com>
2024-05-05 23:07:32 -0400

    test: remove spec file for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
