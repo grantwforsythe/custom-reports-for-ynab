08cecde8ef9d8c6dece69dbf5c7b215ae6fbb022
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2024-05-10 15:56:37 -0400

    feat: add cloudflare web analytics (#15)
    * feat: add analytics service
    
    Create a service to inject the Cloudflare Web Analytics script into the
    DOM as it is only need in production.
    
    * feat: update privacy policy
    
    Update privacy policy with the addition of Cloudflare Web Analytics and
    in preparation for asking my OAuth client ID to be removed from
    restricted mode.
    
    * ci: check for formatting when linting

---
+++ src/app/analytics.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnalyticsService {
+  /**
+   * Injects a script to track web analytics using Cloudflare's beacon.min.js.
+   *
+   * @return {Observable<Event>} An observable that emits an event when the script is successfully injected,
+   * or an error event if the injection fails.
+   */
+  injectScript(): Observable<Event> {
+    return new Observable((observer) => {
+      const script = document.createElement('script');
+      script.defer = true;
+      script.type = 'text/javascript';
+      script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+      script.setAttribute('data-cf-beacon', '{"token": "f35c166c64154ae5822d35b80f4ed71e"}');
+
+      script.onload = () => {
+        observer.next(new Event('Cloudflare Web Analytics injected'));
+        observer.complete();
+      };
+      script.onerror = () => {
+        observer.error('Cloudflare Web Analytics injection failed');
+      };
+
+      document.head.appendChild(script);
+    });
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { DomSanitizer } from '@angular/platform-browser';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
+import { environment as env } from '../environments/environment';
+import { AnalyticsService } from './analytics.service';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from './shared/services/auth/auth.service';
 
@@ -23,8 +25,9 @@ import { AuthService } from './shared/services/auth/auth.service';
   ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   auth = inject(AuthService);
+  analytics = inject(AnalyticsService);
 
   constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     iconRegistry.addSvgIcon(
@@ -35,5 +38,14 @@ export class AppComponent {
       'charts',
       sanitizer.bypassSecurityTrustResourceUrl('assets/images/charts.svg'),
     );
+  }
+
+  ngOnInit() {
+    if (env.production) {
+      this.analytics.injectScript().subscribe({
+        next: console.log,
+        error: console.error,
+      });
+    }
   }
 }
