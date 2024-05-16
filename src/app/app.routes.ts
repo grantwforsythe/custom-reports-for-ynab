ee07eb3c0d3c94f90082537bd37e0dd3d9a5e774
Grant Forsythe <grantwforsythe@gmail.com>
2024-05-15 22:23:38 -0400

    refactor: implement lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,24 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { PrivacyComponent } from './privacy/privacy.component';
-import { BudgetCardsComponent } from './reports/feature/budget-cards/budget-cards.component';
-import { DashboardComponent } from './reports/feature/dashboard/dashboard.component';
 import { authGuard } from './shared/services/auth/auth.guard';
 
-// TODO: Privacy policy route
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'privacy', component: PrivacyComponent },
   // TODO: Refactor to use child routes
   {
     path: 'budgets',
-    component: BudgetCardsComponent,
+    loadComponent: () =>
+      import('./reports/feature/budget-cards/budget-cards.component').then(
+        (m) => m.BudgetCardsComponent,
+      ),
     canActivate: [authGuard],
   },
   {
     path: 'budgets/:id/dashboard',
-    component: DashboardComponent,
+    loadComponent: () =>
+      import('./reports/feature/dashboard/dashboard.component').then((m) => m.DashboardComponent),
     canActivate: [authGuard],
   },
   { path: '**', redirectTo: '' },
